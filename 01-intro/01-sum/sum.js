86f4c15f4870f16d0f163b57e483fa3e8438aa2c
piotrvasilyeu <piotr.vasilyeu@p97.com>
2023-06-29 13:15:51 +0200

    [введение] - описание функции sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
-  /* ваш код */
+    if(typeof a === "number" && typeof b === 'number'){
+        return a + b;
+    }
+
+    throw TypeError('wrong type');
 }
 
 module.exports = sum;
